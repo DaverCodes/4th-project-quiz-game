565d6a4bc80e639b45e2d8e2c97338b38b25bcba
daveS <davesimpcontrol@gmail.com>
2023-05-22 18:13:17 -0600

    changed the question format to note which question is correct from its placement in the index

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,56 +2,84 @@ let currentQuestionIndex = 0;
 let score = 0;
 let timer;
 
-var questions = [
+// var questions = [
+//   {
+//     question: "Commonly used data types DO NOT include:?",
+//     answers: [
+//       { text: "strings", correct: false },
+//       { text: "booleans", correct: false },
+//       { text: "alerts", correct: true },
+//       { text: "numbers", correct: false },
+//     ],
+//   },
+
+//   {
+//     question:
+//       "The condition in an if / else statement is enclosed within ___.",
+//     answers: [
+//       { text: "quotes", correct: false },
+//       { text: "curly brackets", correct: false },
+//       { text: "parenthesis", correct: true },
+//       { text: "square brackets", correct: false },
+//     ],
+//   },
+
+//   {
+//     question: "Arrays in JavaScript can be used to store ___.",
+//     answers: [
+//       { text: "numbers and strings", correct: false },
+//       { text: "other arrays", correct: false },
+//       { text: "booleans", correct: false },
+//       { text: "all of the above", correct: true },
+//     ],
+//   },
+
+//   {
+//     question: "String values must be enclosed within ___ when being assigned to variables.",
+//     answers: [
+//       { text: "commas", correct: false },
+//       { text: "curly brackets", correct: false },
+//       { text: "quotes", correct: true },
+//       { text: "parentheses", correct: false },
+//     ],
+//   },
+
+//   {
+//     question: "a very useful tool used during development and debugging got printing content to the debugger is:",
+//     answers: [
+//       { text: "JavaScript", correct: false },
+//       { text: "terminal / bash", correct: false },
+//       { text: "for loops", correct: false },
+//       { text: "console log", correct: true },
+//     ],
+//   },
+// ];
+ 
+const questions = [
   {
     question: "Commonly used data types DO NOT include:?",
-    answers: [
-      { text: "strings", correct: false },
-      { text: "booleans", correct: false },
-      { text: "alerts", correct: true },
-      { text: "numbers", correct: false },
-    ],
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: 3 // Index of the correct answer in the choices array
   },
-
   {
-    question:
-      "The condition in an if / else statement is enclosed within ___.",
-    answers: [
-      { text: "quotes", correct: false },
-      { text: "curly brackets", correct: false },
-      { text: "parenthesis", correct: true },
-      { text: "square brackets", correct: false },
-    ],
+    question: "The condition in an if / else statement is enclosed within ___.",
+    choices: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+    answer: 3
   },
-
   {
     question: "Arrays in JavaScript can be used to store ___.",
-    answers: [
-      { text: "numbers and strings", correct: false },
-      { text: "other arrays", correct: false },
-      { text: "booleans", correct: false },
-      { text: "all of the above", correct: true },
-    ],
+    choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+    answer: 4
   },
-
   {
     question: "String values must be enclosed within ___ when being assigned to variables.",
-    answers: [
-      { text: "commas", correct: false },
-      { text: "curly brackets", correct: false },
-      { text: "quotes", correct: true },
-      { text: "parentheses", correct: false },
-    ],
+    choices: ["commas", "curly brackets", "quotes", "parentheses"],
+    answer: 3
   },
-
   {
     question: "a very useful tool used during development and debugging got printing content to the debugger is:",
-    answers: [
-      { text: "JavaScript", correct: false },
-      { text: "terminal / bash", correct: false },
-      { text: "for loops", correct: false },
-      { text: "console log", correct: true },
-    ],
+    answers: ["JavaScript", "terminal / bash", "for loops", "console log"],
+    answer: 4
   },
 ];
 
